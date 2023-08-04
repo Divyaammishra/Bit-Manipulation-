57348322cbc699d133c9981334d91ef01b7f3f80
Divyaam Mishra <78863278+Divyaammishra@users.noreply.github.com>
2023-08-04 22:44:26 +0530

    Add files via upload

---
+++ Binary1sComplement/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        Binary 1's complement is also known as NOT operation,
+         */
+
+        System.out.print(~5); // ~ represents NOT operation or 1's compliment
+    }
+}
