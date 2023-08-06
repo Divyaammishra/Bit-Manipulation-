3d69d61af7ff36950be92b1721d2abe0ad82e982
Divyaam Mishra <78863278+Divyaammishra@users.noreply.github.com>
2023-08-06 14:00:53 +0530

    Add files via upload

---
+++ Question2/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    /*
+    Q. Add 1 to an integer by using Bit Manipulation
+    Using NOT operator.
+
+    negative of number invert its bit and add 1 to it. -> int a = 2,  System.out.println(-a); Output = -3;
+
+    -~number will add 1 to an integer and keep it positive, int a = 2,  System.out.println(-~a); Output = 4;
+     */
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        System.out.println();
+    }
+}
